258bb5eb00d2c3555fc5b666db749dd52bedd782
gitemre <emreozturk333@aof.anadolu.edu.tr>
2022-02-14 23:29:56 +0300

    dropdown

---
+++ src/test/java/tests/UpdatedDropdown.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UpdatedDropdown {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Emre\\Selenium\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(2000L);
+        int i = 1;
+       /* while (i < 5) {
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }*/
+
+        for (i = 1; i < 5; i++) {
+            driver.findElement(By.id("hrefIncAdt")).click();
+
+        }
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+    }
+}
